d098929e83c670a7d32da4aa7318e66d7fd10fb5
philiplambok <philiplambok71@gmail.com>
2019-06-20 09:23:01 +0700

    Call app component in home#index to test vue works correctly.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,4 +14,15 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import Vue from "vue/dist/vue.esm";
+import App from "../app.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    el: "#app",
+    data: {
+      message: "Can you say hello?"
+    },
+    components: { App }
+  });
+});
